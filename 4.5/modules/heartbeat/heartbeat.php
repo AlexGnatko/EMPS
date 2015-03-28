41f12d879c953012bd88ebb368c3b9afc9478cdf
Alex Gnatko <gnatko@mail.ru>
2015-03-28 12:27:08 +0800

    Ignore user abort in heartbeat

--- 4.5/modules/heartbeat/heartbeat.php
+++ 4.5/modules/heartbeat/heartbeat.php
@@ -2,6 +2,10 @@
 
 $emps->no_smarty = true;
 
+set_time_limit(0);
+ignore_user_abort(true);
+ini_set('memory_limit', -1);
+
 $data = file_get_contents(EMPS_SCRIPT_WEB."/sendmail/");
 $data = file_get_contents(EMPS_SCRIPT_WEB."/purge_sessions/");
 
