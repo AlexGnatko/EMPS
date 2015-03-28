4bea6b70eb1e87afb8ff53929b6e06006b714f4b
Alex Gnatko <gnatko@mail.ru>
2015-03-28 12:34:16 +0800

    Heartbeat fork

--- 4.5/modules/heartbeat/heartbeat.php
+++ 4.5/modules/heartbeat/heartbeat.php
@@ -6,17 +6,31 @@ set_time_limit(0);
 ignore_user_abort(true);
 ini_set('memory_limit', -1);
 
-$data = file_get_contents(EMPS_SCRIPT_WEB."/sendmail/");
-$data = file_get_contents(EMPS_SCRIPT_WEB."/purge_sessions/");
+$pid = pcntl_fork();
+            
+if ( $pid == -1 ) {       
+    // Fork failed           
+	echo "Fork failed!";
+	exit(1);
+} elseif ( $pid ) {
+    // We are the parent
+	echo "Parent exiting!";
+	exit(0);
+} else {
+    // We are the child
+	$data = file_get_contents(EMPS_SCRIPT_WEB."/sendmail/");
+	$data = file_get_contents(EMPS_SCRIPT_WEB."/purge_sessions/");
 
-$fn = $emps->page_file_name("_heartbeat,project", "controller");
-if(file_exists($fn)){
-	require_once $fn;
+	$fn = $emps->page_file_name("_heartbeat,project", "controller");
+	if(file_exists($fn)){
+		require_once $fn;
+	}
+
+	$fn = $emps->page_file_name("_heartbeat,local", "controller");
+	if(file_exists($fn)){
+		require_once $fn;
+	}
 }
 
-$fn = $emps->page_file_name("_heartbeat,local", "controller");
-if(file_exists($fn)){
-	require_once $fn;
-}
 
 ?>
