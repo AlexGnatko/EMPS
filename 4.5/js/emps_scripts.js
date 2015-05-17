19a4967f0a3f15ae36d8150219ecfa4fe7ca29a0
Alex Gnatko <gnatko@mail.ru>
2015-05-17 13:12:34 +0800

    EMPS scripts callable as a function

--- 4.5/js/emps_scripts.js
+++ 4.5/js/emps_scripts.js
@@ -1,7 +1,11 @@
 // JavaScript Document
 
-var emps_script;
-while(emps_scripts.length > 0){
-	emps_script = emps_scripts.shift();
-	emps_script.call(this);
+function call_emps_scripts(){
+	var emps_script;
+	while(emps_scripts.length > 0){
+		emps_script = emps_scripts.shift();
+		emps_script.call(this);
+	}
 }
+
+call_emps_scripts();
