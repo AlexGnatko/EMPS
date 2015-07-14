27357d053ff22874fd854cbad84d8e3462c52527
Alex Gnatko <gnatko@mail.ru>
2015-07-14 18:47:49 +0800

    Countdown

---
+++ 4.5/js/emps_countdown.js
@@ -0,0 +1,64 @@
+// JavaScript Document
+
+(function( $ ){
+	function EMPS_Countdown(params, object) {	
+		// Constructor function
+		this.params = $.extend(true, this.params, this.defaults);		
+		this.params = $.extend(true, this.params, params);
+		this.object = object;
+		this.initialize();
+	}
+	
+	// Class definition
+    $.extend(EMPS_Countdown.prototype, {
+		defaults: {
+			seconds: 60,
+			off: 'Expired!'
+		},
+		initialize: function(){
+			var data_seconds = $(this.object).data("seconds");
+			if(data_seconds > 0){
+				this.params.seconds = data_seconds;
+			}
+			this.params.start_dt = this.get_time();
+			this.show_formatted();
+		},
+		get_time: function(){
+			return Math.floor(Date.now() / 1000);
+		},
+		two_digits: function(v){
+			var text = v.toString();
+			if(v<10){
+				text = "0" + text;
+			}
+			return text;
+		},
+		show_formatted: function(){
+			var elapsed = this.get_time() - this.params.start_dt;
+			var left = this.params.seconds - elapsed;
+			var text = "";
+			if(left > 0){
+				var seconds = this.two_digits(left % 60);
+				var minutes = this.two_digits(Math.floor(left/60) % 60);
+				var hours = this.two_digits(Math.floor(Math.floor(left / 60) / 60) % 60);
+				text = hours+":"+minutes+":"+seconds;
+			}else{
+				text = this.params.off;
+			}
+			$(this.object).html(text);
+			var that = this;
+			setTimeout(function(){
+				that.show_formatted();
+			}, 1000);
+		}
+	});
+	
+	$.fn.EMPS_Countdown = function(params) {
+		this.each(function() {
+			$(this).data('EMPS_Countdown', new EMPS_Countdown(params, this));
+		});
+		
+		return this;
+    }
+})( jQuery );
+
