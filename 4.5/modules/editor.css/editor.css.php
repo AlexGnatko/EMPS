afefaa51435fddb8d596723e9d15fdb8fb1781c4
Alex Gnatko <gnatko@mail.ru>
2015-09-03 15:05:02 +0800

    Preview button translated

--- 4.5/modules/editor.css/editor.css.php
+++ 4.5/modules/editor.css/editor.css.php
@@ -4,9 +4,13 @@ header("Content-Type: text/css; charset=utf-8");
 
 $emps->no_smarty = true;
 
+echo '/* fonts.css */'."\r\n";
 echo file_get_contents($emps->plain_file("/fonts/fonts.css"));
+echo '/* bootstrap.min.css */'."\r\n";
 echo file_get_contents($emps->plain_file("/css/bootstrap.min.css"));
+echo '/* default.css */'."\r\n";
 echo file_get_contents($emps->plain_file("/css/default.css"));
+echo '/* editor.css */'."\r\n";
 echo file_get_contents($emps->plain_file("/css/editor.css"));
 
 ?>
