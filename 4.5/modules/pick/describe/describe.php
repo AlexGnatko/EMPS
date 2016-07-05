af4813b7e6b54e7753792131d7a9d095b04f5737
Alex Gnatko <gnatko@mail.ru>
2016-07-05 17:13:08 +0800

    picker describe

--- 4.5/modules/pick/describe/describe.php
+++ 4.5/modules/pick/describe/describe.php
@@ -11,14 +11,20 @@ $extra = $x[1];
 $object = $emps->db->get_row($type,"id=".$id);
 
 require_once $emps->common_module('objsel/objsel.class.php');
+require_once $emps->common_module('tables/tables.class.php');
 
 $objsel = new EMPS_ObjectSelector();
+
+$tables = new EMPS_Tables();
 
 if($object){
 	$object['extra'] = "";
 	if($type == 'e_users'){
 		$object['name'] = $object['username'].' - '.$object['fullname'];
 		$object['link'] = "/admin-siteusers/".$object['id']."/-/info/";
+	}
+	if($object['parent']){
+		$object['name'] .= $tables->text_parents($type, $object['parent']);
 	}
 	$fn = $emps->page_file_name('_pick/describe,descr_modifier', 'controller');
 	if(file_exists($fn)){
