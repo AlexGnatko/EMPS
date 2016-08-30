376324c4b463cab16613248eb583c86ce56a7231
Alex Gnatko <gnatko@mail.ru>
2016-08-30 10:02:20 +0800

    photos

--- 4.5/common/ited/pads/photos.php
+++ 4.5/common/ited/pads/photos.php
@@ -2,16 +2,39 @@
 
 $this->handle_view_row();
 
-$emps->page_property("blueimp_uploader",1);
-
 require_once($emps->common_module('photos/blueimp/uploader.class.php'));
 
 $biup = new EMPS_BlueimpUploader;
 
-if(!$this->can_save()){
-	$biup->can_save = false;
-}
 
-$biup->handle_request($this->context_id);
+
+if($_GET['export']){
+	$smarty->assign("ExportMode", 1);
+	
+	$txt = "";
+	$lst = $biup->p->list_pics($this->context_id, 100000);
+	foreach($lst as $pic){
+		if($txt != ""){
+			$txt .= "\r\n";
+		}
+		$txt .= EMPS_SCRIPT_WEB."/pic/".$pic['md5'].".".$pic['ext'];
+	}
+	
+	$smarty->assign("txt", $txt);
+	
+}else{
+
+	$emps->page_property("blueimp_uploader",1);
+	
+	require_once($emps->common_module('photos/blueimp/uploader.class.php'));
+	
+	$biup = new EMPS_BlueimpUploader;
+	
+	if(!$this->can_save()){
+		$biup->can_save = false;
+	}
+	
+	$biup->handle_request($this->context_id);
+}
 
 ?>
