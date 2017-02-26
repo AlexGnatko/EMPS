c7d0236e528d09fc90f7dd6f581dcbd5f68e1851
Alex Gnatko <gnatko@mail.ru>
2017-02-26 18:21:33 +0800

    Debug off

--- common/core/heartbeat.class.php
+++ common/core/heartbeat.class.php
@@ -16,16 +16,13 @@ class EMPS_Heartbeat {
     }
 
     public function execute(){
-//        set_time_limit(60);
+        set_time_limit(60);
 
-        error_log("CURL Execute");
         foreach($this->queue as $url){
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_URL, $url);
             curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
             curl_setopt($ch, CURLOPT_HEADER, 0);
-
-            error_log("Heartbeat: ".$url);
 
             $this->ch[] = $ch;
         }
@@ -33,30 +30,8 @@ class EMPS_Heartbeat {
         $mh = curl_multi_init();
 
         foreach($this->ch as $ch) {
-            error_log("Adding handle");
             curl_multi_add_handle($mh, $ch);
         }
-/*
-        $active = null;
-        do {
-            $mrc = curl_multi_exec($mh, $active);
-            sleep(1);
-            error_log("mrc: ".$mrc);
-            break;
-        } while ($mrc == CURLM_CALL_MULTI_PERFORM);
-
-        while ($active && $mrc == CURLM_OK) {
-            if (curl_multi_select($mh) != -1) {
-                do {
-                    $mrc = curl_multi_exec($mh, $active);
-                    error_log("mrc: ".$mrc);
-                    sleep(1);
-                    break;
-                } while ($mrc == CURLM_CALL_MULTI_PERFORM);
-            }
-            break;
-        }*/
-
 
         $running = 0;
 
