47e768a8db89ce70d723c95803c8b14b422b6f63
Alex Gnatko <gnatko@mail.ru>
2017-07-17 22:46:50 +0800

    Unique texts - work in progress

--- 4.5/modules/admin/uniqtxt/uniqtxt.php
+++ 4.5/modules/admin/uniqtxt/uniqtxt.php
@@ -4,6 +4,7 @@ if($emps->auth->credentials("users")){
     $session_token = $_SESSION['OAUTH_ACCESS_TOKEN'];
     if(isset($session_token['https://oauth.yandex.ru/token'])){
         $smarty->assign("yandex_token", $session_token);
+        dump($session_token);
     }
 }else{
     $emps->deny_access("UserNeeded");
