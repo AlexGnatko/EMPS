96ee424c7d3d79d336a0ebbf7cb4454564977294
Alex Gnatko <gnatko@mail.ru>
2017-07-17 22:50:58 +0800

    Unique texts - work in progress

--- 4.5/modules/admin/uniqtxt/uniqtxt.php
+++ 4.5/modules/admin/uniqtxt/uniqtxt.php
@@ -3,7 +3,17 @@
 if($emps->auth->credentials("users")){
     $session_token = $_SESSION['OAUTH_ACCESS_TOKEN'];
     if(isset($session_token['https://oauth.yandex.ru/token'])){
-        $smarty->assign("yandex_token", $session_token['https://oauth.yandex.ru/token']);
+        $yandex_token = $session_token['https://oauth.yandex.ru/token'];
+        $smarty->assign("yandex_token", $yandex_token);
+
+        if($_GET['capture_token']){
+            $emps->save_setting("yandex_token", json_encode($yandex_token));
+        }
+    }
+    $stored_yandex_token = $emps->get_setting("yandex_token");
+    if($stored_yandex_token){
+        $stored_yandex_token = json_decode($stored_yandex_token, true);
+        $smarty->assign("stored_yandex_token", $stored_yandex_token);
     }
 }else{
     $emps->deny_access("UserNeeded");
