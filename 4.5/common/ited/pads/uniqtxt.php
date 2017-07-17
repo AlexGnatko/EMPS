0e4e6645fbded9e12161c5250e57b11be8b69faf
Alex Gnatko <gnatko@mail.ru>
2017-07-18 00:09:56 +0800

    Unique texts - work in progress

--- 4.5/common/ited/pads/uniqtxt.php
+++ 4.5/common/ited/pads/uniqtxt.php
@@ -5,4 +5,4 @@ $this->handle_view_row();
 require_once $emps->common_module('uniqtxt/uniqtxt.class.php');
 
 $utxt = new EMPS_UniqueTexts;
-$utxt->handle_request($this->context_id, $this->table_name, $this->row);
+$utxt->handle_request($this->context_id, $this->table_name."/".$this->ref_id, $this->row);
