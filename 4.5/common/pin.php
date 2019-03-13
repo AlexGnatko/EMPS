41c609e9e95b9342db097b1e99649d290760c493
Alex Gnatko <agnatko@gmail.com>
2019-03-14 07:40:38 +0800

    New captcha

--- 4.5/common/pin.php
+++ 4.5/common/pin.php
@@ -1,7 +1,7 @@
 <?php
 $emps->no_smarty = true;
 
-$variants = 5;
+$variants = 4;
 
 $pkey = intval($_GET['pkey']);
 $number = "0000000";
@@ -28,7 +28,7 @@ imagefill($im, 0, 0, $transparent);
 $colors = array();
 
 for($i = 0; $i < 8; $i++){
-    $colors[$i] = imagecolorallocatealpha( $im, mt_rand(60, 160), mt_rand(60, 160), mt_rand(60, 160), mt_rand(160, 220));
+    $colors[$i] = imagecolorallocatealpha( $im, mt_rand(60, 160), mt_rand(60, 160), mt_rand(60, 160), 255);
 }
 
 for($i = 0; $i <= 21; $i++){
