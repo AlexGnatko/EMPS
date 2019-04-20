6b1529681e75a57f1c52a3f90eb5bfa822321417
Alex Gnatko <agnatko@gmail.com>
2019-04-20 10:13:16 +0800

    clear captcha pincodes

--- 4.5/common/purge_sessions.php
+++ 4.5/common/purge_sessions.php
@@ -8,13 +8,17 @@ if ($last_purge < (time() - 15 * 60)) {
     $emps->save_setting("_last_session_purge", time());
 
     $dt = time() - 30 * 24 * 60 * 60;
-    $emps->db->query("delete from " . TP . "e_sessions where dt < $dt");
-    $emps->db->query("delete from " . TP . "e_php_sessions where dt < $dt");
+    $emps->db->query("delete from " . TP . "e_sessions where dt < {$dt}");
+    $emps->db->query("delete from " . TP . "e_php_sessions where dt < {$dt}");
 
     $dt = time() - 60 * 15;
-    $emps->db->query("delete from " . TP . "e_php_sessions where (dt = cdt and dt < $dt) or sess_id = ''");
+    $emps->db->query("delete from " . TP . "e_php_sessions where (dt = cdt and dt < {$dt}) or sess_id = ''");
 
-    $r = $emps->db->query("select s.id as sid, b.id as bid from " . TP . "e_browsers as b left join " . TP . "e_php_sessions as s on b.id = s.browser_id having sid is null limit 2000");
+    $dt = time() - 60 * 60 * 24;
+    $emps->db->query("delete from " . TP . "e_pincode where (dt <= {$dt})");
+
+    $r = $emps->db->query("select s.id as sid, b.id as bid from " . TP . "e_browsers as b left join " .
+        TP . "e_php_sessions as s on b.id = s.browser_id having sid is null limit 2000");
     $lst = array();
     while ($ra = $emps->db->fetch_named($r)) {
         $lst[] = $ra['bid'];
