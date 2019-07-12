587e4d167d874c5a084f658259ced24064b1aec1
Alex Gnatko <agnatko@gmail.com>
2019-07-12 13:10:59 +0800

    Import props

--- 4.5/modules/admin/vv/content/common.php
+++ 4.5/modules/admin/vv/content/common.php
@@ -10,3 +10,27 @@ $ited->website_ctx = $emps->website_ctx;
 $ited->add_pad_template("admin/vv/content/pads,%s");
 
 $ited->new_row_fields = ['context_id' => $ited->website_ctx];
+
+if ($_POST['post_import']) {
+    $lst = json_decode($_POST['content_json'], true);
+    foreach ($lst as $v) {
+        $uri = $emps->db->sql_escape($v['uri']);
+        $row = $emps->db->get_row("e_content", "uri = '" . $uri . "' and context_id = " . $emps->website_ctx);
+        $nr = $v;
+        $nr['context_id'] = $emps->website_ctx;
+        if ($row) {
+            $emps->db->sql_update_row("e_content", ['SET' => $nr], "id = " . $row['id']);
+            $id = $row['id'];
+        } else {
+            $emps->db->sql_insert_row("e_content", ['SET' => $nr]);
+            $id = $emps->db->last_insert();
+        }
+        $context_id = $emps->p->get_context($ited->ref_type, $ited->ref_sub, $id);
+        $emps->p->save_properties($v, $context_id, $ited->track_props);
+
+        $ited->v->p->import_photos($context_id, $v['pics']);
+    }
+    $response = [];
+    $response['code'] = "OK";
+    $emps->json_response($response); exit;
+}
--- 4.5/modules/admin/vv/content/content.js
+++ 4.5/modules/admin/vv/content/content.js
@@ -4,9 +4,43 @@ emps_scripts.push(function(){
     vuev = new Vue();
     app = new Vue({
         el: '#content_app',
+        data: function() {
+            return {
+                content_json: '',
+            };
+        },
         mounted: function(){
             $("#content_app").show();
             $(".app-loading").hide();
+        },
+        methods: {
+            open_modal: function(id){
+                vuev.$emit("modal:open:" + id);
+            },
+            close_modal: function(id){
+                $("#" + id).removeClass("is-active");
+            },
+            open_import: function() {
+                this.content_json = '';
+                this.open_modal("importExportModal");
+            },
+            submit_import: function() {
+                var that = this;
+                var row = {};
+                row.post_import = 1;
+                row.content_json = this.content_json;
+                axios
+                    .post("./", row)
+                    .then(function(response){
+                        var data = response.data;
+
+                        if(data.code == 'OK'){
+                            toastr.success(window.string_imported);
+                            vuev.$emit("navigate");
+                        }
+                    });
+                this.close_modal("importExportModal");
+            }
         }
     });
 
