f0cc975b8a450acad77b9b595184362af9a9ef2f
Alex Gnatko <agnatko@gmail.com>
2020-08-05 15:27:58 +0800

    Short MD5, vted update

--- common/modules/test/shortmd5/shortmd5.php
+++ common/modules/test/shortmd5/shortmd5.php
@@ -1,14 +1,19 @@
 <?php
 
-$emps->no_smarty = true;
-$emps->plaintext_response();
+if ($emps->auth->credentials("admin")) {
+    $emps->no_smarty = true;
+    $emps->plaintext_response();
 
-$count = 10000;
+    $count = 10000;
 
-for ($i = 0; $i < $count; $i++) {
-    $md5 = md5(uniqid(microtime(true) + 1));
-    $short = $emps->short_md5($md5);
-    $long = $emps->long_md5($short);
-    $cmp = strcmp($md5, $long);
-    echo "Source MD5: {$md5}, short: {$short}, long: {$long}, cmp: {$cmp}\r\n";
+    for ($i = 0; $i < $count; $i++) {
+        $md5 = md5(uniqid(microtime(true) + 1));
+        $short = $emps->short_md5($md5);
+        $long = $emps->long_md5($short);
+        $cmp = strcmp($md5, $long);
+        echo "Source MD5: {$md5}, short: {$short}, long: {$long}, cmp: {$cmp}\r\n";
+    }
+} else {
+    $emps->deny_access("AdminNeeded");
 }
+
