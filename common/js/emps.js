487f40df6df822fa64b1bdc82c188af957a3fb9f
Alex Gnatko <agnatko@gmail.com>
2020-08-10 17:16:12 +0800

    isIE

--- common/js/emps.js
+++ common/js/emps.js
@@ -257,5 +257,12 @@ var EMPS = {
                     alert(data.message);
                 }
             });
+    },
+    isIE: function() {
+        ua = navigator.userAgent;
+        /* MSIE used to detect old browsers and Trident used to newer ones*/
+        var is_ie = ua.indexOf("MSIE ") > -1 || ua.indexOf("Trident/") > -1;
+
+        return is_ie;
     }
 }
