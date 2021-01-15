61b82398329e873fcbd86b6304be79e0f0335367
Alex Gnatko <agnatko@gmail.com>
2021-01-15 12:35:12 +0800

    Show context id

--- 4.5/modules/comp/files/files.php
+++ 4.5/modules/comp/files/files.php
@@ -5,5 +5,6 @@ require_once $emps->common_module('files/vue/uploader.class.php');
 
 $uploader = new EMPS_VuePhotosUploader;
 $uploader->context_id = $context_id;
+$smarty->assign("context_id", $uploader->context_id);
 
 $uploader->handle_request();
--- 4.5/modules/comp/photos/photos.php
+++ 4.5/modules/comp/photos/photos.php
@@ -6,4 +6,6 @@ require_once $emps->common_module('photos/vue/uploader.class.php');
 $uploader = new EMPS_VuePhotosUploader;
 $uploader->context_id = $context_id;
 
+$smarty->assign("context_id", $uploader->context_id);
+
 $uploader->handle_request();
