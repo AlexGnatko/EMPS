3abd85246a82cb5d4ffe0f8d34fc0d592e4c413e
Alex Gnatko <agnatko@gmail.com>
2021-02-01 23:18:59 +0800

    Cache cleaning

--- 4.5/common/purge_sessions.php
+++ 4.5/common/purge_sessions.php
@@ -7,6 +7,9 @@ $last_purge = intval($emps->get_setting("_last_session_purge"));
 if ($last_purge < (time() - 15 * 60)) {
     $emps->save_setting("_last_session_purge", time());
 
+    $dt = time() - 7 * 24 * 60 * 60;
+    $emps->db->query("delete from " . TP . "e_cache where dt < {$dt}");
+
     $dt = time() - 30 * 24 * 60 * 60;
     $emps->db->query("delete from " . TP . "e_sessions where dt < {$dt}");
     $emps->db->query("delete from " . TP . "e_php_sessions where dt < {$dt}");
