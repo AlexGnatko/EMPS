bc46647d3417e4c14ed1ead5f694f36bc0cf590c
Alex Gnatko <agnatko@gmail.com>
2021-04-21 13:33:53 +0800

    SMSC

--- 4.5/common/sms/sms.class.php
+++ 4.5/common/sms/sms.class.php
@@ -6,13 +6,20 @@ class EMPS_SMS
     public $auth_token;
     public $from;
 
+    public $mode = "twilio";
+
     public $error_message = "";
+
+    private $post_mode = "application/x-www-form-urlencoded";
+    private $url = "https://smsc.ru/sys/send.php";
 
     public function __construct()
     {
         $this->account_sid = TWILIO_SID;
         $this->auth_token = TWILIO_TOKEN;
         $this->from = TWILIO_FROM;
+        $this->login = SMSC_LOGIN;
+        $this->password = SMSC_PASSWORD;
     }
 
     public function enqueue_message($to, $msg)
@@ -36,7 +43,7 @@ class EMPS_SMS
         return true;
     }
 
-    public function send_message($to, $msg)
+    public function send_twilio_message($to, $msg)
     {
         $rv = true;
         try {
@@ -55,6 +62,90 @@ class EMPS_SMS
         unset($client);
 
         return $rv;
+    }
+
+    protected function curl_opts(&$ch)
+    {
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_PROTOCOLS, CURLPROTO_HTTP | CURLPROTO_HTTPS);
+        curl_setopt($ch, CURLOPT_REDIR_PROTOCOLS, CURLPROTO_HTTP | CURLPROTO_HTTPS);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+        return true;
+    }
+
+    protected function default_http_headers()
+    {
+        return [
+            'Content-Type: ' . $this->post_mode
+        ];
+    }
+
+    private function data_to_string($data)
+    {
+        $queryString = array();
+        foreach ($data as $param => $value) {
+            if (is_string($value) || is_int($value) || is_float($value)) {
+                $queryString[] = urlencode($param) . '=' . urlencode($value);
+            } elseif (is_array($value)) {
+                foreach ($value as $valueItem) {
+                    $queryString[] = urlencode($param) . '=' . urlencode($valueItem);
+                }
+            } else {
+                continue;
+            }
+        }
+        return implode('&', $queryString);
+    }
+
+    function post($data)
+    {
+        $this->post_mode = "application/x-www-form-urlencoded";
+        $url = $this->url;
+        $headers = $this->default_http_headers();
+        $data['login'] = $this->login;
+        $data['psw'] = $this->password;
+        $data['fmt'] = 3;
+        $data_string = $this->data_to_string($data);
+
+        $ch = curl_init($url);
+        $this->curl_opts($ch);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        $response = json_decode($response, true);
+
+        if (!is_array($response)) {
+            return $this->error('Unknown error in post');
+        }
+
+        return $response;
+    }
+
+    public function send_smsc_message($to, $msg) {
+        $rv = $this->post(['phones' => $to, 'mes' => $msg]);
+        error_log("SMSC rv: {$rv}");
+        $rv = json_decode($rv, true);
+        if (isset($rv['error'])) {
+            $this->error_message = $rv['error'];
+            return false;
+        }
+        return true;
+    }
+
+    public function send_message($to, $msg)
+    {
+        if ($this->mode == "twilio") {
+            return $this->send_twilio_message($to, $msg);
+        }
+        if ($this->mode == "smsc") {
+            return $this->send_smsc_message($to, $msg);
+        }
     }
 
     public function plain_phone($phone)
