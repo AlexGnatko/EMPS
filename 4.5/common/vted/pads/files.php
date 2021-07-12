323aed59485a86100b86afb80944bbf6bd4083f8
Alex Gnatko <agnatko@gmail.com>
2021-07-12 21:20:27 +0800

    Upload context

--- 4.5/common/vted/pads/files.php
+++ 4.5/common/vted/pads/files.php
@@ -1,7 +1,9 @@
 <?php
 
-global $context_id, $vted;
+global $context_id, $vted, $smarty;
 
 $context_id = $vted->context_id;
+
+$smarty->assign("context_id", $context_id);
 
 require_once $emps->page_file_name("_comp/files", "controller");
