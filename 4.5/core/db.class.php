59590b32280e66830f81b3199c740ea6df37a96f
Alex Gnatko <agnatko@gmail.com>
2021-09-23 15:09:52 +0800

    $in query

--- 4.5/core/db.class.php
+++ 4.5/core/db.class.php
@@ -234,6 +234,9 @@ class EMPS_DB
                     } elseif (isset($v['$like'])) {
                         $value = $v['$like'];
                         $part .= " like ('$value') ";
+                    } elseif (isset($v['$in'])) {
+                        $value = $v['$in'];
+                        $part .= " in ($value) ";
                     } elseif (isset($v['$not'])) {
                         $value = $v['$not'];
                         $part .= " <> $value ";
